8f73679e3546849dc1d1a1f9f82ef4c2044b897a
Harini Vasudevan <harini.vasudevan@oracle.com>
2022-03-26 09:53:11 -0400

    LL

--- datastructure/app.js
+++ datastructure/app.js
@@ -1,5 +1,12 @@
-const LinkedList = require("../datastructure/linkedlist");
+const LinkedList = require("./linkedlist");
 
 const ll = LinkedList.fromValues(10, 20, 30, 40);
 console.log(`value==>${ll.getByIndex(2).data}`);
+ll.insertAtIndex(3, 100000);
+ll.print();
+
+//ll.removeHead();
+//ll.print();
+
+ll.removeAtIndex(3);
 ll.print();
--- datastructure/linkedList.js
+++ datastructure/linkedList.js
@@ -4,76 +4,63 @@ class LinkedList {
         this.length = 0;
     }
 
-    /*----------------------------------------------------------------
-	LinkedList {
-		head: LinkedListNode { data: 'Hello', next: null },
-		length: 1
-		}
-		LinkedList {
-		head: LinkedListNode {
-			data: '2',
-			next: LinkedListNode { data: 'Hello', next: null }
-		},
-		length: 2
-		}
-	*/
     insertAtHead (data) {
-        const newNode = new LinkedListNode(data, this.head);
+        let currentNode = this.head;
+        const newNode = new LinkedListNode(data, currentNode);
         this.head = newNode;
         this.length++;
     }
-
     getByIndex (index) {
-        if (index < 0 || index > this.length) return null;
-
         let current = this.head;
         for (let i = 0; i < index; i++) {
-            current = current.next; //this is the node at index
+            current = current.next;
         }
         return current;
     }
+    insertAtIndex (index, data) {
+        if (index === 0) return this.insertAtHead(data);
 
+        let prevNode = this.getByIndex(index - 1);
+        if (prevNode == null) {
+            return null;
+        }
+        let currentNode = this.getByIndex(index);
+        let newNode = new LinkedListNode(data, currentNode.next);
+        prevNode.next = newNode;
+        newNode.next = currentNode;
+        this.length++;
+        this.print();
+    }
+
+    removeHead () {
+        this.head = this.head.next;
+        this.length--;
+    }
+
+    removeAtIndex (index) {
+        if (index == null) {
+            return this.removeHome();
+        }
+        this.print();
+        let prevNode = this.getByIndex(index - 1);
+        let currentNode = this.getByIndex(index);
+        prevNode.next = currentNode.next;
+        this.length--;
+        this.print();
+    }
     print () {
         let output = "";
         let current = this.head;
         while (current) {
-            output = `${output}${current.data} ->`;
+            output = `${output}${current.data}->`;
             current = current.next;
         }
-        console.log(`${output}null`);
-    }
-
-    insertAtIndex_old (...newnodeparams) {
-        const [index, data] = newnodeparams;
-
-        if (index < 0 || index == null) {
-            return null;
-        }
-
-        console.log(`Inside LL..insertAtIndex...data=${data},index=${index}`);
-        let prevNode = this.getByIndex(index - 1);
-
-        let currentNodeAtIndex = this.getByIndex(index);
-        let newNode = new LinkedListNode(data, currentNodeAtIndex.next);
-        prevNode.next = newNode;
-        newNode.next = currentNodeAtIndex;
-        console.log(`CurrentnNode=${currentNodeAtIndex}`);
-        this.print();
-    }
-
-    insertAtIndex (index, data) {
-        const prevNode = this.getByIndex(index - 1); //prevNode.data & prevNode.next should point to the new node
-        const currentNode = this.getByIndex(index); // newNode's next should point to the current node
-        const newNode = new LinkedListNode(data, currentNode.next);
-        prevNode.next = newNode;
-        newNode.next = currentNode;
-        this.print();
+        console.log(`${output}->null`);
     }
 }
 
 class LinkedListNode {
-    constructor (data, next, ll) {
-        console.log(`Inside LLN...data=${data},next=${next}`);
+    constructor (data, next) {
         this.data = data;
         this.next = next;
     }
@@ -83,13 +70,7 @@ LinkedList.fromValues = function (...values) {
     const ll = new LinkedList();
     for (let i = values.length - 1; i >= 0; i--) {
         ll.insertAtHead(values[i]);
-        console.log(i, ll);
     }
-    return ll;
-};
-
-LinkedList.addAtIndex = function (ll, index, data) {
-    ll.insertAtIndex(index, data);
     return ll;
 };
 
