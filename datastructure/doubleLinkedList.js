98d9a87a917e63a16745f85bdc55be42194cfa9d
Harini Vasudevan <harini.vasudevan@oracle.com>
2022-03-27 17:19:34 -0400

    Double LL

--- datastructure/doubleLinkedList.js
+++ datastructure/doubleLinkedList.js
@@ -1,67 +1,110 @@
-const LinkedList = require("./linkedList");
-
-class DoubleLinkedList {
-    constructor () {
-        this.head = null;
-        this.tail = null;
-        this.length = null;
+class DoubelLinkedList {
+    constructor (init) {
+        this.head = new DoubleLinkedListNode(init);
+        this.tail = this.head;
+        this.length = 0;
     }
 
-    insertAtHead (data) {
-        if (!this.head) {
-        }
+    insertAtHead (value) {
+        let newNode = new DoubleLinkedListNode(value);
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
         this.length++;
     }
-    print () {
+
+    insertAtTail (value) {
+        let newNode = new DoubleLinkedListNode(value);
+        this.tail.prev = newNode;
+        newNode.next = this.tail;
+        this.tail = newNode;
+        this.length++;
+    }
+
+    getListByIndex (index) {
+        let arr = [];
         let current = this.head;
+        let i = 0;
+        while (current !== null) {
+            arr.push(current);
+            current = current.next;
+        }
+        console.log(`Value at index: ${arr[index]}`);
+        return arr[index];
+    }
+
+    getListByIndexFromTail (index) {
+        let arr = [];
+        let current = this.tail;
+        let i = 0;
+        while (current !== null) {
+            arr.push(current.value);
+            current = current.prev;
+        }
+        console.log(`Value at index: ${arr[this.length - 1 - index]}`);
+        return arr[this.length - 1 - index];
+    }
+
+    printfromTail () {
+        let current = this.tail;
+        let arr = [];
         let output = "";
         while (current !== null) {
-            output = `${output}${current.data}->`;
+            output = `${output}<--${current.value}`;
+            current = current.prev;
+        }
+        output = `null${output}`;
+        console.log(output);
+    }
+
+    printfromhead () {
+        let current = this.head;
+        let arr = [];
+        let output = "";
+        while (current !== null) {
+            output = `${output}${current.value}->`;
             current = current.next;
         }
         output = `${output}null`;
         console.log(output);
     }
-    printFromTail () {
-        let current = this.tail;
-        let output = "";
-        while (current !== null) {
-            output = `${output}<-${current.data}`;
-            current = current.prev;
+
+    removeFirst () {
+        let current = this.head;
+        //let prevNode = current.prev; //always null
+        let nextNode = current.next;
+        nextNode.prev = current;
+        this.head = nextNode;
+    }
+    removeNode (index) {
+        if (index == 0) {
+            this.removeFirst();
         }
-        output = `null${output}`;
-        console.log(`${output}`);
+        let node = this.getListByIndex(index);
+        console.log(node.value);
+        let prevNode = node.prev;
+        let nextNode = node.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
     }
 }
 
 class DoubleLinkedListNode {
-    constructor (data, prevnode = null, nextnode = null) {
-        this.data = data;
-        this.prev = prevnode;
-        this.next = nextnode;
+    constructor (value) {
+        this.value = value;
+        this.prev = null;
+        this.next = null;
     }
 }
 
-let dll = new DoubleLinkedList();
-console.log(dll);
-exit();
-dll.print();
-dll.printFromTail();
-dll.insertAtHead("10");
-dll.print();
-dll.printFromTail();
-
-dll.insertAtHead("20");
-dll.print();
-dll.printFromTail();
-
-dll.insertAtHead("30");
-dll.print();
-dll.printFromTail();
-
-dll.insertAtHead("40");
-dll.print();
-dll.printFromTail();
-
-console.log("-------");
-dll.printFromTail();
+const dll = new DoubelLinkedList("head");
+dll.insertAtHead(10);
+dll.insertAtHead(20);
+dll.insertAtHead(30);
+dll.printfromhead();
+dll.printfromTail();
+dll.getListByIndex(0);
+dll.getListByIndex(dll.length - 1);
+dll.getListByIndexFromTail(1);
+dll.removeNode(0);
+dll.printfromhead();
